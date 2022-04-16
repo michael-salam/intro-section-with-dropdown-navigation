7b680c6c4bd91fcdb96cd9dfd32ab7dad67cc212
michael-salam <84015713+michael-salam@users.noreply.github.com>
2022-04-16 20:38:04 +0100

    Updated script.js with comments

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,9 @@
 // select all elements with class hover
 const dropdownContainers = document.querySelectorAll(".has-dropdown");
+// select dropdowns
 const dropdown1 = document.querySelector("#dropdown1");
 const dropdown2 = document.querySelector("#dropdown2");
+// select arrows-icons
 const arrow1 = document.querySelector("#arrow1");
 const arrow2 = document.querySelector("#arrow2");
 
@@ -17,6 +19,7 @@ dropdownContainers.forEach(function (element) {
 function handleToggleDropdown(e) {
   if (e.target.id === "has-dropdown1") {
     if (!dropdown2.classList.contains("invisible")) {
+      // if first dropdown is clicked, reset the styles for the other
       dropdown2.classList.add("invisible");
       arrow2.classList.replace("arrow-up", "arrow-down");
     }
@@ -29,6 +32,7 @@ function handleToggleDropdown(e) {
     }
   } else if (e.target.id === "has-dropdown2") {
     if (!dropdown1.classList.contains("invisible")) {
+      // if second dropdown is clicked, reset the styles for the other
       dropdown1.classList.add("invisible");
       arrow1.classList.replace("arrow-up", "arrow-down");
     }
@@ -40,14 +44,16 @@ function handleToggleDropdown(e) {
       arrow2.classList.replace("arrow-up", "arrow-down");
     }
   }
-
-  //   console.log(dropdown2.style.display);
 }
 
 function handleCloseAllDropdowns(e) {
+  // use conditional to make sure what is being clicked is not either of the dropdowns
   if (e.target.id !== "has-dropdown1" && e.target.id !== "has-dropdown2") {
+    // make both dropdowns disappear
     dropdown1.classList.add("invisible");
     dropdown2.classList.add("invisible");
+
+    // make both arrows face down
     arrow1.classList.replace("arrow-up", "arrow-down");
     arrow2.classList.replace("arrow-up", "arrow-down");
   }
