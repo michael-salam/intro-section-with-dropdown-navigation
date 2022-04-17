e7c1e76140a7377a02c76d52c2e09c9f0ffb11cb
michael-salam <84015713+michael-salam@users.noreply.github.com>
2022-04-17 15:21:56 +0100

    Built mobile menu and completed project

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,31 @@
-// select all elements with class hover
-const dropdownContainers = document.querySelectorAll(".has-dropdown");
-// select dropdowns
-const dropdown1 = document.querySelector("#dropdown1");
-const dropdown2 = document.querySelector("#dropdown2");
-// select arrows-icons
-const arrow1 = document.querySelector("#arrow1");
-const arrow2 = document.querySelector("#arrow2");
-
+// CONSTANTS DEFINED BELOW
 const body = document.querySelector("body");
 
+// select all elements with class hover
+const dropdownContainers = document.querySelectorAll(".has-dropdown");
+
+// select dropdowns
+const dropdown1 = document.getElementById("dropdown1");
+const dropdown2 = document.getElementById("dropdown2");
+
+// select arrows-icons
+const arrow1 = document.getElementById("arrow1");
+const arrow2 = document.getElementById("arrow2");
+
+// select the menu icon
+const menu = document.getElementById("menu");
+
+// select the menu icon
+const menuIcon = document.getElementById("menu-icon");
+
+// select the close icon
+const closeIcon = document.getElementById("close-icon");
+
+// select the overlay
+const overlay = document.getElementById("overlay");
+
+// EVENT LISTENERS ADDED BELOW
+// add click event listener to body
 body.addEventListener("click", handleCloseAllDropdowns);
 
 // add hover event listener to all elements with class hover
@@ -16,6 +33,13 @@ dropdownContainers.forEach(function (element) {
   element.addEventListener("click", handleToggleDropdown);
 });
 
+// add click event listeners to each of the menu icon, close icon and overlay
+menuIcon.addEventListener("click", handleOpenMobileMenu);
+//you can close the mobile menu by either clicking the close icon or the overlay
+closeIcon.addEventListener("click", handleCloseMobileMenu);
+overlay.addEventListener("click", handleCloseMobileMenu);
+
+// FUNCTIONS DEFINED BELOW
 function handleToggleDropdown(e) {
   if (e.target.id === "has-dropdown1") {
     if (!dropdown2.classList.contains("invisible")) {
@@ -57,4 +81,15 @@ function handleCloseAllDropdowns(e) {
     arrow1.classList.replace("arrow-up", "arrow-down");
     arrow2.classList.replace("arrow-up", "arrow-down");
   }
+}
+
+function handleOpenMobileMenu() {
+  menu.style.left = "36.2vw";
+  overlay.classList.remove("invisible");
+  body.classList.add("no-scroll");
+}
+function handleCloseMobileMenu() {
+  menu.style.left = "50rem";
+  overlay.classList.add("invisible");
+  body.classList.remove("no-scroll");
 }
